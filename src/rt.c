dfd599b271944f1748b765c953759bd4b5b672b8
Carlos Dominguez <cdominguezm@gmail.com>
2016-05-10 17:57:34 +0200

    Purge() method implemented tested and working.

--- src/rt.c
+++ src/rt.c
@@ -99,6 +99,18 @@ rt_put (rt_t *self, bios_proto_t **msg_p)
     bios_proto_destroy(msg_p);
 }
 
+void
+rt_purge (rt_t *self, const char *device, const char *metric){
+    rt_t *dev = NULL;
+    dev =(rt_t*) zhashx_lookup(self->hash, device);
+    if(dev){
+        zhashx_delete(dev->hash, metric);
+	if(zhashx_size(dev->hash) == 0){
+	  rt_destroy(&dev);
+	  zhashx_delete(self->hash, device);
+	}
+    }
+}
 //  --------------------------------------------------------------------------
 //  Print
 void
@@ -217,6 +229,8 @@ rt_test (bool verbose)
     rt_put (self, &metric);
     assert (metric == NULL); // Make sure message is deleted
 
+    rt_purge(self,"swtich","load");
+    
     rt_print (self); // test print
 
     rt_destroy (&self);
