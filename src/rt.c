f05f043199deac96f952f50a0561de2af17d0c92
Carlos Dominguez <cdominguezm@gmail.com>
2016-05-11 11:21:51 +0200

    test

--- src/rt.c
+++ src/rt.c
@@ -88,7 +88,7 @@ rt_put (rt_t *self, bios_proto_t **msg_p)
     if(!device){
         device = rt_new();
         assert(device);
-	//device->hash = zhashx_new();
+	device->hash = zhashx_new();
 	assert(device->hash);
 	zhashx_set_destructor(device->hash, (zhashx_destructor_fn *) rt_destroy);
 	device->print = rt_print_metrics;
