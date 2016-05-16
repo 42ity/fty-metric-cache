ff63eb8d959b3095b712f2e424210546b91db4f4
Karol Hrdina <karolhrdina@eaton.com>
2016-05-16 14:27:48 +0200

    PROBLEM: Changes in protocol are not implemented. SOLUTION: Implement changes
    PROBLEM: Protocol is not tested.
    SOLUTION: add tests

--- src/mailbox.c
+++ src/mailbox.c
@@ -46,7 +46,8 @@ mailbox_perform (mlm_client_t *client, zmsg_t **msg_p, rt_t *data)
     // check subject
     if (!streq (mlm_client_subject (client), RFC_RT_DATA_SUBJECT)) {
         zmsg_destroy (msg_p);
-        log_warning ("Message with bad subject received. Sender: '%s', Subject: '%s'.",
+        log_warning (
+                "Message with bad subject received. Sender: '%s', Subject: '%s'.",
                 mlm_client_sender (client), mlm_client_subject (client));
         return;
     }
@@ -55,8 +56,8 @@ mailbox_perform (mlm_client_t *client, zmsg_t **msg_p, rt_t *data)
     if (!uuid) {
         zmsg_destroy (msg_p);
         log_warning (
-            "Bad message. Expected multipart string message `uuid/GET/element`"
-            " - 'uuid' field missing. Sender: '%s', Subject: '%s'.",
+                "Bad message. Expected multipart string message `uuid/GET/element`"
+                " - 'uuid' field missing. Sender: '%s', Subject: '%s'.",
                 mlm_client_sender (client), mlm_client_subject (client));
         return;
     }
@@ -67,8 +68,8 @@ mailbox_perform (mlm_client_t *client, zmsg_t **msg_p, rt_t *data)
         zstr_free (&command);        
         zstr_free (&uuid);
         log_warning (
-            "Bad message. Expected multipart string message `uuid/GET/element`"
-            " - 'GET' missing or different string. Sender: '%s', Subject: '%s'.",
+                "Bad message. Expected multipart string message `uuid/GET/element`"
+                " - 'GET' missing or different string. Sender: '%s', Subject: '%s'.",
                 mlm_client_sender (client), mlm_client_subject (client));
         return;
     }
@@ -79,8 +80,8 @@ mailbox_perform (mlm_client_t *client, zmsg_t **msg_p, rt_t *data)
         zmsg_destroy (msg_p);
         zstr_free (&uuid);
         log_warning (
-            "Bad message. Expected multipart string message `uuid/GET/element`"
-            " - 'GET' missing or different string. Sender: '%s', Subject: '%s'.",
+                "Bad message. Expected multipart string message `uuid/GET/element`"
+                " - 'GET' missing or different string. Sender: '%s', Subject: '%s'.",
                 mlm_client_sender (client), mlm_client_subject (client));
         return;
     }
@@ -92,23 +93,23 @@ mailbox_perform (mlm_client_t *client, zmsg_t **msg_p, rt_t *data)
     zmsg_addstr (reply, element);
 
     zhashx_t *hash = rt_get_element (data, element);
-    zframe_t *frame = NULL;
-    if (!hash) {
-        hash = zhashx_new ();
-        frame = zhashx_pack (hash);
-        zhashx_destroy (&hash);
+    if (hash) { 
+        bios_proto_t *metric = (bios_proto_t *) zhashx_first (hash);
+        while (metric) {
+            bios_proto_t *copy = bios_proto_dup (metric);
+            zmsg_t *encoded = bios_proto_encode (&copy);
+            zmsg_addmsg (reply, &encoded);
+            metric = (bios_proto_t *) zhashx_next (hash);
+        }
     }
-    else {
-        frame = zhashx_pack (hash);
-    }
-    zmsg_append (reply, &frame);
 
     zstr_free (&uuid);
     zstr_free (&element);
 
-    int rv = mlm_client_sendto (client, mlm_client_sender (client), RFC_ALERTS_LIST_SUBJECT, NULL, 5000, &reply);
+    int rv = mlm_client_sendto (client, mlm_client_sender (client), RFC_RT_DATA_SUBJECT, NULL, 5000, &reply);
     if (rv != 0) {
-        log_error ("mlm_client_sendto (sender = '%s', subject = '%s', timeout = '5000') failed.",
+        log_error (
+                "mlm_client_sendto (sender = '%s', subject = '%s', timeout = '5000') failed.",
                 mlm_client_sender (client), RFC_RT_DATA_SUBJECT);
     }
 }
@@ -116,12 +117,210 @@ mailbox_perform (mlm_client_t *client, zmsg_t **msg_p, rt_t *data)
 //  --------------------------------------------------------------------------
 //  Self test of this class
 
+static bios_proto_t *
+test_metric_new (
+        const char *type,
+        const char *element,
+        const char *value,
+        const char *unit,
+        uint32_t ttl
+        )
+{
+    bios_proto_t *metric = bios_proto_new (BIOS_PROTO_METRIC);
+    bios_proto_set_type (metric, "%s", type);
+    bios_proto_set_element_src (metric, "%s", element);
+    bios_proto_set_unit (metric, "%s", unit);
+    bios_proto_set_value (metric, "%s", value);
+    bios_proto_set_ttl (metric, ttl);
+    return metric;
+}
+
+static void
+test_assert_proto (
+        bios_proto_t *p,
+        const char *type,
+        const char *element,
+        const char *value,
+        const char *unit,
+        uint32_t ttl)
+{
+    assert (p);
+    assert (streq (bios_proto_type (p), type));
+    assert (streq (bios_proto_element_src (p), element));
+    assert (streq (bios_proto_unit (p), unit));
+    assert (streq (bios_proto_value (p), value));
+    assert (bios_proto_ttl (p) == ttl);
+}
+
 void
 mailbox_test (bool verbose)
 {
-    printf (" * mailbox: ");
+    static const char* endpoint = "inproc://bios-agent-rt-mailbox-test";
 
+    printf (" * mailbox: ");
     //  @selftest
+ 
+    // Malamute
+    zactor_t *server = zactor_new (mlm_server, "Malamute");
+    zstr_sendx (server, "BIND", endpoint, NULL);
+    if (verbose)
+        zstr_send (server, "VERBOSE");
+
+    // User Interface
+    mlm_client_t *ui = mlm_client_new ();
+    mlm_client_connect (ui, endpoint, 1000, "UI");
+
+    // Mailbox peer
+    mlm_client_t *mailbox = mlm_client_new ();
+    mlm_client_connect (mailbox, endpoint, 1000, "MAILBOX");
+
+    // data, fill
+    rt_t *data = rt_new ();
+    bios_proto_t *metric = test_metric_new ("temp", "ups", "15", "C", 100);
+    rt_put (data, &metric);
+    metric = test_metric_new ("humidity", "ups", "40", "%", 200);
+    rt_put (data, &metric);
+    metric = test_metric_new ("battery.remaining", "ups", "20", "%", 200);
+    rt_put (data, &metric);
+    metric = test_metric_new ("humidity", "epdu", "21", "%", 100);
+    rt_put (data, &metric);
+    metric = test_metric_new ("load.input", "switch", "134", "V", 200);
+    rt_put (data, &metric);
+    metric = test_metric_new ("amperes", "switch", "50", "A", 300);
+    rt_put (data, &metric);
+
+    // ===============================================
+    // Test case #1:
+    //      GET ups
+    // Expected:
+    //      3 measurements
+    // ===============================================
+    zmsg_t *send = zmsg_new ();
+    zmsg_addstr (send, "12345");
+    zmsg_addstr (send, "GET");
+    zmsg_addstr (send, "ups");
+    int rv = mlm_client_sendto (ui, "MAILBOX", RFC_RT_DATA_SUBJECT, NULL, 5000, &send);
+    assert (rv == 0);
+
+    zmsg_t *reply = mlm_client_recv (mailbox);
+    assert (reply);
+    mailbox_perform (mailbox, &reply, data);
+    reply = mlm_client_recv (ui);
+    assert (reply);
+    assert (streq (mlm_client_subject (ui), RFC_RT_DATA_SUBJECT));
+
+    char *uuid = zmsg_popstr (reply);
+    assert (uuid);
+    assert (streq (uuid, "12345"));
+    zstr_free (&uuid);
+
+    char *command = zmsg_popstr (reply);
+    assert (command);
+    assert (streq (command, "OK"));
+    zstr_free (&command);
+    
+    char *element = zmsg_popstr (reply);
+    assert (element);
+    assert (streq (element, "ups"));
+    zstr_free (&element);
+ 
+    zmsg_t *encoded = zmsg_popmsg (reply);
+    assert (encoded);
+    bios_proto_t *proto = bios_proto_decode (&encoded);
+    test_assert_proto (proto, "battery.remaining", "ups", "20", "%", 200);
+    bios_proto_destroy (&proto);
+
+    encoded = zmsg_popmsg (reply);
+    assert (encoded);
+    proto = bios_proto_decode (&encoded);
+    test_assert_proto (proto, "temp", "ups", "15", "C", 100);
+    bios_proto_destroy (&proto);
+
+    encoded = zmsg_popmsg (reply);
+    assert (encoded);
+    proto = bios_proto_decode (&encoded);
+    test_assert_proto (proto, "humidity", "ups", "40", "%", 200);
+
+    bios_proto_destroy (&proto);
+    
+    encoded = zmsg_popmsg (reply);
+    assert (encoded == NULL);
+    zmsg_destroy (&reply);
+
+    // End Test case #1
+
+    // ===============================================
+    // Test case #2:
+    //      Send bad message
+    // Expected:
+    //      no reply
+    // ===============================================
+    send = zmsg_new ();
+    zmsg_addstr (send, "GET");
+    zmsg_addstr (send, "ups");
+    rv = mlm_client_sendto (ui, "MAILBOX", RFC_RT_DATA_SUBJECT, NULL, 5000, &send);
+    assert (rv == 0);
+
+    reply = mlm_client_recv (mailbox);
+    assert (reply);
+    mailbox_perform (mailbox, &reply, data);
+
+    zsys_debug ("Waiting in zpoller for 5000ms");
+    zpoller_t *poller = zpoller_new (mlm_client_msgpipe (ui), NULL);
+    void *which = zpoller_wait (poller, 5000);
+    assert (which == NULL);
+    assert (zpoller_expired (poller));
+
+    zpoller_destroy (&poller);
+  
+    // End Test case #2   
+
+    // ===============================================
+    // Test case #3:
+    //      GET non-existant-element
+    // Expected:
+    //      0 measurements
+    // ===============================================
+    send = zmsg_new ();
+    zmsg_addstr (send, "8cb3e9a9-649b-4bef-8de2-25e9c2cebb38");
+    zmsg_addstr (send, "GET");
+    zmsg_addstr (send, "non-existant-element");
+    rv = mlm_client_sendto (ui, "MAILBOX", RFC_RT_DATA_SUBJECT, NULL, 5000, &send);
+    assert (rv == 0);
+
+    reply = mlm_client_recv (mailbox);
+    assert (reply);
+    mailbox_perform (mailbox, &reply, data);
+    reply = mlm_client_recv (ui);
+    assert (reply);
+    assert (streq (mlm_client_subject (ui), RFC_RT_DATA_SUBJECT));
+
+    uuid = zmsg_popstr (reply);
+    assert (uuid);
+    assert (streq (uuid, "8cb3e9a9-649b-4bef-8de2-25e9c2cebb38"));
+    zstr_free (&uuid);
+
+    command = zmsg_popstr (reply);
+    assert (command);
+    assert (streq (command, "OK"));
+    zstr_free (&command);
+    
+    element = zmsg_popstr (reply);
+    assert (element);
+    assert (streq (element, "non-existant-element"));
+    zstr_free (&element);
+
+    encoded = zmsg_popmsg (reply);
+    assert (encoded == NULL);
+    zmsg_destroy (&reply);
+
+    // End Test case #3
+
+    rt_destroy (&data);
+    mlm_client_destroy (&ui);
+    mlm_client_destroy (&mailbox);
+    zactor_destroy (&server);
+
     //  @end
-    printf ("Empty test - OK\n");
+    printf ("OK\n");
 }
