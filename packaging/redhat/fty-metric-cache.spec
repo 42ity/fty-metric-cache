dd9d1d5e6516ba366a06d0a38d489ee335a04709
bstepankova <bstepankova@volny.cz>
2016-12-14 16:11:39 +0100

    Problem: We need to apply new naming convention
    Solution: renamed

---
+++ packaging/redhat/fty-metric-cache.spec
@@ -0,0 +1,100 @@
+#
+#    fty-metric-cache - Knows current values of any METRIC in the system
+#
+#    Copyright (C) 2014 - 2015 Eaton                                        
+#                                                                           
+#    This program is free software; you can redistribute it and/or modify   
+#    it under the terms of the GNU General Public License as published by   
+#    the Free Software Foundation; either version 2 of the License, or      
+#    (at your option) any later version.                                    
+#                                                                           
+#    This program is distributed in the hope that it will be useful,        
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of         
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+#    GNU General Public License for more details.                           
+#                                                                           
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+#
+
+Name:           fty-metric-cache
+Version:        1.0.0
+Release:        1
+Summary:        knows current values of any metric in the system
+License:        MIT
+URL:            http://example.com/
+Source0:        %{name}-%{version}.tar.gz
+Group:          System/Libraries
+BuildRequires:  automake
+BuildRequires:  autoconf
+BuildRequires:  libtool
+BuildRequires:  pkg-config
+BuildRequires:  systemd-devel
+BuildRequires:  zeromq-devel
+BuildRequires:  czmq-devel
+BuildRequires:  malamute-devel
+BuildRequires:  libfty_proto-devel
+BuildRoot:      %{_tmppath}/%{name}-%{version}-build
+
+%description
+fty-metric-cache knows current values of any metric in the system.
+
+%package -n libfty_metric_cache1
+Group:          System/Libraries
+Summary:        knows current values of any metric in the system
+
+%description -n libfty_metric_cache1
+fty-metric-cache knows current values of any metric in the system.
+This package contains shared library.
+
+%post -n libfty_metric_cache1 -p /sbin/ldconfig
+%postun -n libfty_metric_cache1 -p /sbin/ldconfig
+
+%files -n libfty_metric_cache1
+%defattr(-,root,root)
+%doc COPYING
+%{_libdir}/libfty_metric_cache.so.*
+
+%package devel
+Summary:        knows current values of any metric in the system
+Group:          System/Libraries
+Requires:       libfty_metric_cache1 = %{version}
+Requires:       zeromq-devel
+Requires:       czmq-devel
+Requires:       malamute-devel
+Requires:       libfty_proto-devel
+
+%description devel
+fty-metric-cache knows current values of any metric in the system.
+This package contains development files.
+
+%files devel
+%defattr(-,root,root)
+%{_includedir}/*
+%{_libdir}/libfty_metric_cache.so
+%{_libdir}/pkgconfig/libfty_metric_cache.pc
+
+%prep
+%setup -q
+
+%build
+sh autogen.sh
+%{configure} --with-systemd-units
+make %{_smp_mflags}
+
+%install
+make install DESTDIR=%{buildroot} %{?_smp_mflags}
+
+# remove static libraries
+find %{buildroot} -name '*.a' | xargs rm -f
+find %{buildroot} -name '*.la' | xargs rm -f
+
+%files
+%defattr(-,root,root)
+%{_bindir}/fty-metric-cache
+%{_bindir}/fty-metric-cache-cli
+%{_prefix}/lib/systemd/system/fty-metric-cache*.service
+
+
+%changelog
---
+++ src/fty_metric_cache_cli.c
@@ -0,0 +1,163 @@
+/*  =========================================================================
+    fty_metric_cache_cli - Command line interface for agent-rt
+
+    Copyright (C) 2014 - 2015 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    =========================================================================
+*/
+
+/*
+@header
+     fty_metric_cache_cli - Command line interface for agent-rt
+@discuss
+@end
+*/
+
+#include "fty_metric_cache_classes.h"
+#include <time.h>
+
+const char *endpoint = "ipc://@/malamute";
+zpoller_t *poller = NULL;
+
+void print_device(const char *device, mlm_client_t *cli){
+    zmsg_t *send = zmsg_new ();
+    zmsg_addstr (send, "");
+    zmsg_addstr (send, "GET");
+    zmsg_addstr (send, device);
+
+    int rv = mlm_client_sendto (cli, "agent-rt", RFC_RT_DATA_SUBJECT, NULL, 5000, &send);
+    assert (rv == 0);
+}
+
+void list_devices(mlm_client_t *cli){
+    zmsg_t *send = zmsg_new ();
+    zmsg_addstr (send, "");
+    zmsg_addstr (send, "LIST");
+
+    int rv = mlm_client_sendto (cli, "agent-rt", RFC_RT_DATA_SUBJECT, NULL, 5000, &send);
+    assert (rv == 0);
+}
+
+zmsg_t *
+reciver (mlm_client_t *client, int timeout)
+{
+    if (zsys_interrupted)
+        return NULL;
+
+    if (!poller)
+        poller = zpoller_new (mlm_client_msgpipe (client), NULL);
+
+    zsock_t *which = (zsock_t *) zpoller_wait (poller, timeout);
+    if (which == mlm_client_msgpipe (client)) {
+        zmsg_t *reply = mlm_client_recv (client);
+        return reply;
+    }
+    return NULL;
+}
+
+int main (int argc, char *argv [])
+{
+
+    mlm_client_t *client = mlm_client_new ();
+    if ( !client ) {
+        zsys_error ("agent-rt-cli:\tlm_client_new memory error");
+        return -1;
+    }
+
+    int rv = mlm_client_connect (client, endpoint, 1000, "CLI");
+    if ( rv == -1 ) {
+        zsys_error ("agent-rt-cli:\tCannot connect to malamute on '%s'", endpoint);
+        mlm_client_destroy (&client);
+        return -1;
+    }
+
+    bool verbose = false;
+    for (int argn = 1; argn < argc; argn++) {
+        if (    streq (argv [argn], "--help")
+             || streq (argv [argn], "-h"))
+        {
+            puts ("agent-rt-cli [options]");
+            puts ("agent-rt-cli [device]    print all information about the device");
+            puts ("  --list / -l            print list of devices known to the agent");
+            puts ("  --verbose / -v         verbose output");
+            puts ("  --help / -h            this information");
+            break;
+        }
+        else
+        if (    streq (argv [argn], "--verbose")
+             || streq (argv [argn], "-v"))
+        {
+            verbose = true;
+        }
+        else
+        if (    streq (argv [argn], "--list")
+             || streq (argv [argn], "-l"))
+        {
+            list_devices(client);
+            break;
+        }
+        else {
+            print_device(argv [argn], client);
+            break;
+        }
+    }
+
+    if (verbose)
+        zsys_info ("agent_rt_cli - Command line interface for agent-rt");
+
+    zmsg_t *msg = reciver (client, 1000);
+    if (msg) {
+        char *uuid = zmsg_popstr (msg);
+        char *confirmation = zmsg_popstr (msg);
+        char *command = zmsg_popstr (msg);
+        char *reply = NULL;
+        if(streq(command, "LIST")){
+            reply = zmsg_popstr (msg);
+            printf ("%s", reply);
+            free (reply);
+        }else{
+            printf("Device: %s\n", command);
+            char _bufftime[sizeof "YYYY-MM-DDTHH:MM:SSZ"];
+            zmsg_t *msg_part = zmsg_popmsg(msg);
+            fty_proto_t *fty_p_element;
+            while(msg_part){
+                fty_p_element = fty_proto_decode(&msg_part);
+                uint64_t _time = fty_proto_aux_number (fty_p_element, "time", 0);
+                strftime(_bufftime, sizeof _bufftime, "%FT%TZ", gmtime((const time_t*)&_time));
+                printf("%s(ttl=%" PRIu32"s) %20s@%s = %s%s\n",
+                        _bufftime,
+                        fty_proto_ttl (fty_p_element),
+                        fty_proto_type (fty_p_element),
+                        fty_proto_element_src (fty_p_element),
+                        fty_proto_value (fty_p_element),
+                        fty_proto_unit (fty_p_element));
+                fty_proto_destroy(&fty_p_element);
+                msg_part = zmsg_popmsg(msg);
+            }
+        }
+        free (uuid);
+        free (confirmation);
+        free (command);
+
+        zmsg_destroy (&msg);
+        zclock_sleep (100);
+    }
+    else
+        zsys_error ("No agent response");
+
+    mlm_client_destroy (&client);
+    return 0;
+}
